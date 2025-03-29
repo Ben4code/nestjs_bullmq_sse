936f433745c381a5219c73c1d89b81605f55cd15
Nnaemeka Obioha <nnaemeka.obioha@gmail.com>
2025-03-29 12:39:29 -0400

    added prisma init migration

--- apps/backend/src/app/app.service.ts
+++ apps/backend/src/app/app.service.ts
@@ -1,8 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { DatabaseService } from './database';
 
 @Injectable()
 export class AppService {
+  constructor(private readonly databaseService: DatabaseService){}
   getData(): { message: string } {
     return { message: 'Hello API' };
+  }
+
+  async createUser(){
+    const user = await this.databaseService.user.create({data: {
+      email: 'nnaemeka@email22.com',
+      name: 'Nnaemeka Obi'
+    }})
+      
+    return user
   }
 }
